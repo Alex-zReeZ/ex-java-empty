8ccb2d3c3dc81d595e697adc9e59466407d0e2c5
Alex-zReeZ <alexbeck220@gmail.com>
2023-11-27 16:39:32 +0100

    All exercice done

--- src/fetch/fetchData.js
+++ src/fetch/fetchData.js
@@ -1,21 +1,15 @@
 /**
  * In javascript we can use the fetch function to make HTTP calls to the network
  * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
- * In the next exercise, when the user clicks ont the button with id "click-to-fetch"
+ * In the next exercise, when the user clicks on the button with id "click-to-fetch"
  * you have to call this url : https://api.github.com/octocat
  * Then, you have to display the content returned by the api on the "pre" tag with id "display-here"
  */
-export async function fetchDataOnClick() {
-    const url = 'https://api.github.com/octocat';
-
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Given data:', data);
-        })
+export function fetchDataOnClick() {
+    const getElement = document.querySelector("#click-to-fetch")
+    const showHere = document.querySelector("#display-here")
+    getElement.addEventListener("click", async () => {
+        const request = await fetch("https://api.github.com/octocat")
+        showHere.innerText = await request.text()
+    })
 }
